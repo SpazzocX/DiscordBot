a1abc59d7f1b17fa5d117ef7b4cfa347a12632c5
SpazzocX <mahirsteam5@gmail.com>
2025-09-20 01:31:33 +0200

    zamjena za novu verziju

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once("ready", () => {
+client.once("clientReady", () => {
     console.log(`✅ Bot online as ${client.user.tag}`);
 });
 
