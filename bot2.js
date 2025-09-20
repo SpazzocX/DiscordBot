e8d36a197f7c186955112f6c230333b21de9861b
SpazzocX <mahirsteam5@gmail.com>
2025-09-20 02:12:22 +0200

    god of pain

--- bot2.js
+++ bot2.js
@@ -1,26 +1,16 @@
 require("dotenv").config();
 const { Client, GatewayIntentBits } = require("discord.js");
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-client.once("clientReady", () => {
+client.once("ready", () => {
     console.log(`🤖 Bot2 online as ${client.user.tag}`);
 });
 
-// jednostavna random komanda
-client.on("interactionCreate", async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-
-    if (interaction.commandName === "dice") {
-        const roll = Math.floor(Math.random() * 6) + 1;
-        await interaction.reply(`🎲 You rolled a ${roll}!`);
-    }
-});
 client.on("messageCreate", msg => {
     if (msg.content.toLowerCase() === "ping") {
         msg.reply("Pong!");
     }
 });
-
 
 client.login(process.env.DISCORD_TOKEN_BOT2);
