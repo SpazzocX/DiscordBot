869b07ba3cd46bfa613bf98b9ee745925a951191
SpazzocX <mahirsteam5@gmail.com>
2025-09-20 02:14:52 +0200

    ping and fix

--- bot1.js
+++ bot1.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
 const { Client, GatewayIntentBits } = require("discord.js");
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.once("clientReady", () => {
-    console.log(`✅ Bot online as ${client.user.tag}`);
+    console.log(`🤖 Bot2 online as ${client.user.tag}`);
+});
+
+client.on("messageCreate", msg => {
+    if (msg.content.toLowerCase() === "ping") {
+        msg.reply("Pong!");
+    }
 });
 
 client.login(process.env.DISCORD_TOKEN);
